3422569b6dc16a81306cdf30c74e151ef595ad60
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-08-11 11:48:36 +0530

    created

--- app.js
+++ app.js
@@ -16,11 +16,37 @@ app.post("/signUp",(req,res)=>{
     let hashedpassword=bcrypt.hashSync(input.password,12)
     input.password=hashedpassword
     console.log(input)
-
     let result=new loginModel(input)
     result.save()
     res.json({"status":"success"})
 })
+app.post("/adminsignin",(req,res)=>{
+    let input=req.body
+    let result=loginModel.find({username:input.username}).then(
+        (response)=>{
+            if (response.length>0) {
+                const validator=bcrypt.compareSync(input.password,response[0].password)
+                if(validator)
+                {
+                    jwt.sign({email:input.username},"doctor-app",{expiresIn:"1d"},
+                    (error,token)=>{
+                        if (error) {
+                            res.json({"status":"Token Creation Failed"})
+                        } else {
+                            res.json({"status":"success","token":token})
+                        }
+                    })
+                }else
+                {
+                    res.json({"status":"Wrong Password"})
+                }
+            } else {
+                res.json({"status":"Invalid Authentication"}) 
+            }
+        }
+    ).catch()
+})
+
 
 app.listen(8080,()=>{
     console.log("server started")
